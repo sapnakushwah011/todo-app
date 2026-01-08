8499fc4b935a174cabe2d37bd0097e6eec1153dc
Sapna Kushwah <129735749+sapnakushwah011@users.noreply.github.com>
2026-01-08 15:39:00 +0530

    created todo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [input, setInput] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
+  const [editValue, setEditValue] = useState("");
+
+  const handleAdd = () => {
+    if (!input.trim()) return;
+    setItems([...items, input]);
+    setInput("");
+  };
+
+  const handleEdit = (index) => {
+    setEditIndex(index);
+    setEditValue(items[index]); // pre-fill input
+  };
+
+  const handleSave = (index) => {
+    if (!editValue.trim()) return;
+
+    const updatedItems = [...items];
+    updatedItems[index] = editValue;
+    setItems(updatedItems);
+
+    setEditIndex(null);
+    setEditValue("");
+  };
+
+  const handleDelete = (index) => {
+    setItems(items.filter((_, i) => i !== index));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex", flexDirection:"column", justifyContent: "center", alignItems: "center", gap: "10px"}}>
+      <h1>Todo App</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Add todo"
+      />
+      <button onClick={handleAdd}>Add</button>
+
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+            {editIndex === index ? (
+              <>
+                <input
+                  type="text"
+                  value={editValue}
+                  onChange={(e) => setEditValue(e.target.value)}
+                />
+                <button onClick={() => handleSave(index)}>
+                  Save Changes
+                </button>
+              </>
+            ) : (
+              <>
+                {item}
+                <button onClick={() => handleEdit(index)}>Edit</button>
+                <button onClick={() => handleDelete(index)}>Delete</button>
+              </>
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
